d6154f4da8e53b9e5e0b9ddfc6d6d01277fa524a
crmollinedo <cristianm184@hotmail.com>
2023-01-02 22:46:59 -0400

    Create Interface and DAO

---
+++ app/src/main/java/com/example/roomdemo/db/SubscriberDAO.kt
@@ -0,0 +1,28 @@
+package com.example.roomdemo.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
+interface SubscriberDAO {
+
+    @Insert
+    suspend fun insertSubscriber(subscriber: Subscriber)
+
+    @Update
+    suspend fun updateSubscriber(subscriber: Subscriber)
+
+    @Delete
+    suspend fun deleteSubscriber(subscriber: Subscriber)
+
+    @Query("DELETE FROM SUBSCRIBER_DATA_TABLE")
+    suspend fun deleteAll()
+
+    @Query("SELECT * FROM SUBSCRIBER_DATA_TABLE")
+    fun getAllSubscribers():LiveData<List<Subscriber>>
+
+}
---
+++ app/src/main/java/com/example/roomdemo/db/SubscriberDataBase.kt
@@ -0,0 +1,32 @@
+package com.example.roomdemo.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+
+@Database(entities = [Subscriber::class],version = 1)
+abstract class SubscriberDataBase : RoomDatabase() {
+
+    companion object{
+
+        @Volatile
+        private var INSTANCE : SubscriberDataBase? = null
+        fun getInstance(context: Context) : SubscriberDataBase{
+            synchronized(this){
+                var instance : SubscriberDataBase? = INSTANCE
+                if(instance==null){
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        SubscriberDataBase::class.java,
+                        "susbcriber_data_database"
+                    ).build()
+                    INSTANCE = instance
+                }
+            return instance
+            }
+        }
+
+    }
+}
